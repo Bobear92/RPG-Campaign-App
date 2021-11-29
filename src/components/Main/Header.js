8ea601735823fe3b47dbc149fb5fccc76964c320
Robert Woolbright <robwoolbright@gmail.com>
2021-11-28 19:44:42 -0500

    working on drop down links in header

--- src/components/Main/Header.js
+++ src/components/Main/Header.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
+
 import "./Header.css";
 import { Login, Register } from ".";
 import { getUser } from "../../auth";
@@ -14,41 +15,68 @@ const Header = ({ loggedIn, setLoggedIn }) => {
     <div className="header-main-container">
       {loggedIn ? (
         <>
-          <NavLink className="nav-button" to="/">
-            Home
-          </NavLink>
-          <div className="log-reg-container">
-            <NavLink className="nav-button" to="/my-info">{`${user}`}</NavLink>
-            <NavLink
-              className="nav-button"
-              to="/"
-              onClick={() => {
-                localStorage.clear();
-                setLoggedIn(false);
-              }}
-            >
-              Log Out
+          <div className="header-top-container">
+            <NavLink className="nav-button" to="/">
+              Home
             </NavLink>
+            <div className="log-reg-container">
+              <NavLink
+                className="nav-button"
+                to="/my-info"
+              >{`${user}`}</NavLink>
+              <NavLink
+                className="nav-button"
+                to="/"
+                onClick={() => {
+                  localStorage.clear();
+                  setLoggedIn(false);
+                }}
+              >
+                Log Out
+              </NavLink>
+            </div>
+          </div>
+
+          <div className="drop-down-container">
+            <form>
+              <label className="drop-down-label">
+                Character
+                <select className="drop-down-options">
+                  <option>Select</option>
+                  <option>Character Sheet</option>
+                  <option>Class</option>
+                  <option>Sub-Class</option>
+                  <option>Feats</option>
+                  <option>SpellList</option>
+                  <option>SpellBook</option>
+                </select>
+              </label>
+            </form>
           </div>
         </>
       ) : (
         <>
-          <NavLink className="nav-button" to="/">
-            Home
-          </NavLink>
-          <div className="log-reg-container">
-            <div className="login-container">
-              <button className="nav-button" onClick={() => setLogToggle(true)}>
-                Login
-              </button>
-              {logToggle ? <Login setLoggedIn={setLoggedIn} /> : null}
-            </div>
-            <div
-              className="register-container"
-              onClick={() => setRegisterToggle(true)}
-            >
-              <button className="nav-button">Register</button>
-              {registerToggle ? <Register setLoggedIn={setLoggedIn} /> : null}
+          <div className="header-top-container">
+            <NavLink className="nav-button" to="/">
+              Home
+            </NavLink>
+            <div className="log-reg-container">
+              <div className="login-container">
+                <button
+                  className="nav-button"
+                  onClick={() => setLogToggle(true)}
+                >
+                  Login
+                </button>
+                {logToggle ? <Login setLoggedIn={setLoggedIn} /> : null}
+              </div>
+              <div
+                className="register-container"
+                onClick={() => setRegisterToggle(true)}
+              >
+                <button className="nav-button">Register</button>
+                {registerToggle ? <Register setLoggedIn={setLoggedIn} /> : null}
+              </div>
             </div>
           </div>
         </>

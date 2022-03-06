27e70698038c24447935acccc50259914fc2483d
Robert Woolbright <robwoolbright@gmail.com>
2022-03-06 17:32:06 -0500

    edited home brew rules so players can only see visible rules

--- src/components/Mechanics/HomeBrewRules.jsx
+++ src/components/Mechanics/HomeBrewRules.jsx
@@ -1,24 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
 import "./HomeBrewRules.css";
 
 const HomeBrewRules = ({ allMyRules }) => {
-  console.log(allMyRules);
   return (
     <div className="home-brew-rules-main-component">
       <h1>Home Brew Rules</h1>
-      {allMyRules && allMyRules.length
-        ? allMyRules.map((rule, idx) => {
-            return (
-              <div
-                className="home-brew-rules-rule-map-component"
-                key={`rule inside of rules map in home brew rules ${rule.name} ${idx}`}
-              >
-                <h3>{rule.name}</h3>
-                <p>{rule.description}</p>
-              </div>
-            );
-          })
-        : null}
+      <div className="home-brew-rules-rule-map-main-component">
+        {allMyRules && allMyRules.length
+          ? allMyRules.map((rule, idx) => {
+              return rule.visible ? (
+                <div
+                  className="home-brew-rules-rule-map-component"
+                  key={`rule inside of rules map in home brew rules ${rule.name} ${idx}`}
+                >
+                  <h3>{rule.name}</h3>
+                  <p>{rule.description}</p>
+                </div>
+              ) : null;
+            })
+          : null}
+      </div>
     </div>
   );
 };

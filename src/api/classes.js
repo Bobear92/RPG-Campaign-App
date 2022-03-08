f19ab9ca667ba9aca8e1dbc763a2a19cd257d6cb
Robert Woolbright <robwoolbright@gmail.com>
2022-03-08 15:16:32 -0500

    switching api back to development

--- src/api/classes.js
+++ src/api/classes.js
@@ -1,4 +1,4 @@
 import axios from "axios";
 
-// const BASE = "http://localhost:5000/api/"; // When in development have this one running
-const BASE = "https://rpg-campaign-app.herokuapp.com/api/"; // when deploying to heroku have this one running
+const BASE = "http://localhost:5000/api/"; // When in development have this one running
+// const BASE = "https://rpg-campaign-app.herokuapp.com/api/"; // when deploying to heroku have this one running
